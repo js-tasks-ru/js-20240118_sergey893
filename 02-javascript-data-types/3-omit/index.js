d9ec91f95f4138b791171fcc8fb7eeeb5b478046
Sergey Klimov <ksiu@pkzdrav.ru>
2024-01-15 15:36:28 +0700

    task of omit added

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let clone = {};
+  for (const [key, value] of Object.entries(obj)) {
+    if (!fields.includes(key)) {
+      clone[key] = value;
+    }
+  }
+  return clone;
 };
