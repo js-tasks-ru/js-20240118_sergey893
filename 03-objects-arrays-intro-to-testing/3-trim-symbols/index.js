b0be91c68e32cc7aba2f68004f960ca3de2a390c
Sergey Klimov <ksiu@pkzdrav.ru>
2024-01-23 11:32:35 +0700

    Task 3.3 completed

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  
+  if (size === undefined) {
+    return string;
+  }
 
+  if (size === 0) {
+    return '';
+  }
+
+  let prevChar = string.charAt(0);
+  let matches = 1;
+  let result = prevChar;
+
+  for (let i = 1; i < string.length; i++) {
+    if (prevChar == string.charAt(i)) {      
+      matches++;
+      if (matches <= size) {
+        result += string.charAt(i);
+      }
+    } else {
+      matches = 1;
+      result += string.charAt(i);
+    }
+    prevChar = string.charAt(i);
+  }
+  
+  return result;
 }
