2f5a42170c035d0751493e68b660b92be1fb8d40
Sergey Klimov <ksiu@pkzdrav.ru>
2024-02-02 14:03:14 +0700

    class NotificationMessage initialized

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,36 @@
-export default class NotificationMessage {
+// export default class NotificationMessage {
+class NotificationMessage {
+  // подсказка 1  - см. видео 41:40 про требование "без DOM"
+  static lastInstance;
+
+  constructor(prop) {
+    this.prop = prop;
+  }
+
+  show() {
+    if (NotificationMessage.lastInstance) {
+      NotificationMessage.lastInstance.remove();
+    }
+    
+    // add to DOM
+    NotificationMessage.lastInstance = this;
+    console.log('lastInstance', NotificationMessage.lastInstance);    
+  }
+
+  remove () {
+    this.lastInstance = undefined;
+  }
+
+  destroy () {
+    this.remove();
+  }
 
 }
+
+const obj1 = new NotificationMessage(1);
+const obj2 = new NotificationMessage(2);
+const obj3 = new NotificationMessage(3);
+console.log('---');
+obj1.show();
+obj2.show();
+obj3.show();
