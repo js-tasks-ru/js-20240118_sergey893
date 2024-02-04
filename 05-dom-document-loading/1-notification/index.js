bab4c3822ffec8d3b7cb847996ce16c3047e9c92
sergey_klimov <s.klimov@bk.ru>
2024-02-04 17:49:49 +0700

    NotificationMessage drafted

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,36 +1,53 @@
-// export default class NotificationMessage {
-class NotificationMessage {
+export default class NotificationMessage {
   // подсказка 1  - см. видео 41:40 про требование "без DOM"
   static lastInstance;
+  element;
 
-  constructor(prop) {
-    this.prop = prop;
+  constructor(greeteing, { duration = 0, type_ = 'success' }) {
+    this.greeteing = greeteing;
+    this.duration = duration + "ms";
+    this.type_ = type_;
+    this.notificationClass = type_ == 'success' ? 'notification success' : 'notification error';
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
   }
 
   show() {
     if (NotificationMessage.lastInstance) {
       NotificationMessage.lastInstance.remove();
     }
-    
+
+    this.element = this.createElement(this.createTemplate());
+
     // add to DOM
     NotificationMessage.lastInstance = this;
-    console.log('lastInstance', NotificationMessage.lastInstance);    
+    console.log('lastInstance', NotificationMessage.lastInstance);
   }
 
-  remove () {
+  createTemplate() {
+    return (`
+    <div class="${this.notificationClass}" style="--value:${this.duration}">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type_}</div>
+        <div class="notification-body">
+          ${this.greeteing}
+        </div>
+      </div>
+    </div>
+    `);
+  }
+
+  remove() {
     this.lastInstance = undefined;
   }
 
-  destroy () {
+  destroy() {
     this.remove();
   }
 
 }
-
-const obj1 = new NotificationMessage(1);
-const obj2 = new NotificationMessage(2);
-const obj3 = new NotificationMessage(3);
-console.log('---');
-obj1.show();
-obj2.show();
-obj3.show();
