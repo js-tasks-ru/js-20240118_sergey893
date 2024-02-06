938b432dcadd64cbbbd81efccaf4b5f6c6500451
Sergey Klimov <ksiu@pkzdrav.ru>
2024-02-06 17:17:02 +0700

    added a sorting method

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -15,6 +15,19 @@ export default class SortableTable {
     return element.firstElementChild;
   }
 
+  sort (fieldValue, orderValue) {
+    const sortType = this.headers.findIndex(x => x.id === fieldValue)?.sortType;
+
+    if (sortType == 'string') {
+      this.data = sortStrings(this.data, fieldValue, orderValue);  
+    } else {
+      this.data = sortNumbers(this.data, fieldValue, orderValue);
+    }
+
+    this.data = sortStrings(this.data, fieldValue, orderValue);
+    this.element = this.createElement(this.createTemplate());
+  }
+
   createTemplate() {
     return `
       <div data-element="productsContainer" class="products-list__container">
@@ -95,12 +108,17 @@ export default class SortableTable {
   }
 }
 
-function pick(obj, ...fields) {
-  const clone = {};
-  for (const [key, value] of Object.entries(obj)) {
-    if (fields.includes(key)) {
-      clone[key] = value;
-    }
-  }
-  return clone;
+
+function sortStrings(arr, field, param = 'asc') {
+  let collator = new Intl.Collator('ru', { caseFirst: 'upper' });
+  return arr.slice().sort(
+    (a, b) => param == 'asc' ? collator.compare(a[field], b[field]) : collator.compare(b[field], a[field]),
+  );
+}
+
+
+function sortNumbers(arr, field, param = 'asc') {
+  return arr.slice().sort(
+    (a, b) => param == 'asc' ? a[field] - b[field] : b[field] - a[field],
+  );
 }
